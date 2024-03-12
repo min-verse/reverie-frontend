27c866da616ce13479510661d37408e883e1df94
Min Tu <biffboffkev@gmail.com>
2024-03-12 09:44:17 -0400

    adding sessions config file

---
+++ app/services/session.server.ts
@@ -0,0 +1,15 @@
+import { createCookieSessionStorage } from "@remix-run/node";
+
+// Exporting the whole sessionStorage Object
+export let sessionStorage = createCookieSessionStorage({
+    cookie: {
+        name: "sessionId",
+        sameSite: "lax", // in production, this should be 'Strict'
+        path: "/",
+        httpOnly: true, // Enabling only HTTP Cookies
+        secrets: ["blahblahblah"], // Should be an actual secret in .env
+        secure: false, // Enable this as true in production
+    }
+});
+
+export let { getSession, commitSession, destroySession } = sessionStorage;
